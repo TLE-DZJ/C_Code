c1405edcb00f107a34634663a3806affa668ee44
TLE-DZJ <102839767+TLE-DZJ@users.noreply.github.com>
2022-04-04 10:02:27 +0800

    猜数字

--- test.c
+++ test.c
@@ -1,25 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+//随机生产1-100的随机数，然后猜
 int main()
 {
-	//n的阶乘
 	int i = 0;
-	int n = 0;
-	int m = 1;
-	printf("请输入一个数n：");
-	scanf("%d", &n);
-	if (n == 0)
+	//拿时间戳设置随机数的生成起点
+	srand((unsigned int)time(NULL));
+	while(1)
 	{
-		m = 1;
-		printf("n的阶乘为：%d\n", m);
-	}
-	else
-	{
-		for(i=1;i<=n;i++)
+		//生成1-100的随机数
+		int r = rand() % 100 + 1;
+		printf("请输入您要猜的数字：");
+		scanf("%d", &i);
+		printf("随机数为%d\n", r);
+		if (i == r)
 		{
-			m =m*i;
+			printf("正确\n");
+			break;
 		}
-		printf("n的阶乘为：%d\n", m);
+		else
+		{
+			printf("错误\n");
+		}
 	}
 	return 0;
 }
