def8f762ea43ef5b6d4d9ea96cb7e7d141e4985e
TLE-DZJ <102839767+TLE-DZJ@users.noreply.github.com>
2022-04-08 09:16:21 +0800

    Add files via upload

---
+++ 汉诺塔.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void move(char x, char y)
+{
+	printf("%c-->%c\n", x, y);
+}
+
+void hanoi(int n, char one, char two, char three)
+{
+	if (n == 1)
+	{
+		move(one, three);
+	}
+	else
+	{
+		hanoi(n - 1, one, three, two);
+		move(one, three);
+		hanoi(n - 1, two, one, three);
+	}
+}
+
+int main()
+{
+	int m;
+	printf("请输入盘子个数：");
+	scanf("%d", &m);
+	printf("移动盘子的步骤为：\n");
+	hanoi(m, 'A','B','C');
+	return 0;
+}
